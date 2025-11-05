2804644d07d32a2aa391a0d4306932a1245d2fd8
qsenn69 <ksenialukina07@mail.ru>
2025-11-06 01:31:57 +0300

    refactor: remove direct Page usage from tests

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 from utils.config import Config
 from pages.header import Header
 from utils.data import Data
@@ -12,36 +12,36 @@ def test_search(header: Header):
     header.select_end_date(end_form)
     header.click_button_search()
     header.assert_url(f"{Config.BASE_URL}?params=MOW{start_url}TPE{end_url}1&with_request=true")
-
-def test_origin_valid_city(header: Header, page: Page):
+    
+def test_origin_valid_city(header: Header):
     header.fill_origin("Москва")
-    expect(page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Москва")
+    expect(header.page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Москва")
 
-def test_destination_valid_city(header: Header, page: Page):
+def test_destination_valid_city(header: Header):
     header.fill_destination("Иркутск")
-    expect(page.locator(HeaderSelectors.DESTINATION_INPUT)).to_have_value("Иркутск")
+    expect(header.page.locator(HeaderSelectors.DESTINATION_INPUT)).to_have_value("Иркутск")
 
-def test_change_origin_destination(header: Header, page: Page):
+def test_change_origin_destination(header: Header):
     header.fill_origin("Москва")
     header.fill_destination("Фукуок")
-    page.get_by_role("option", name="Фукуок").first.click()
+    header.page.get_by_role("option", name="Фукуок").first.click()
     header.change_origin_destination()
-    expect(page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Фукуок")
-    expect(page.locator(HeaderSelectors.DESTINATION_INPUT)).to_have_value("Москва")
+    expect(header.page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Фукуок")
+    expect(header.page.locator(HeaderSelectors.DESTINATION_INPUT)).to_have_value("Москва")
 
-def test_origin_invalid_city(header: Header, page: Page):
+def test_origin_invalid_city(header: Header):
     header.fill_origin("Ксилоран")
-    expect(page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Ксилоран")
+    expect(header.page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("Ксилоран")
     expect(header.page.get_by_text(HeaderSelectors.NO_RESULTS_DROPDOWN)).to_be_visible()
 
-def test_check_calendar_open(header: Header, page: Page):  
+def test_check_calendar_open(header: Header):  
     header.page.locator(HeaderSelectors.START_DATE_FIELD).click()
-    expect(page.locator(HeaderSelectors.CALENDAR_DROPDOWN)).to_be_visible()
+    expect(header.page.locator(HeaderSelectors.CALENDAR_DROPDOWN)).to_be_visible()
 
 def test_open_settings(header: Header): 
     header.navigate_to_settings()
     header.assert_url(f"{Config.BASE_URL}my/settings")
 
-def test_fill_code_IATA(header: Header, page: Page):
+def test_fill_code_IATA(header: Header):
     header.fill_origin("MSQ")
-    expect(page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("MSQ")
+    expect(header.page.locator(HeaderSelectors.ORIGIN_INPUT)).to_have_value("MSQ")
